2a05a1b5df2fa2037a2921315974cc3ffa6359d3
AndrMoura <andre_ptos29@msn.com>
2023-11-07 12:35:57 +0000

    Refactor makePostRequest function to handle errors properly

--- index.js
+++ index.js
@@ -7,28 +7,29 @@ async function setupPlugin({ config }) {
 }
 
 async function makePostRequest(url, data) {
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Connection": "keep-alive",
-          "Accept": "*/*",
-           
-        },
-        body: JSON.stringify(data)
-      });
-  
-      if (response.status === 200) {
-        const responseData = await response.json();
-        return responseData;
-      } else {
-        console.error("Request status " + response.status);
-      }
-    } catch (error) {
-      console.error("Error:", error);
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.status === 200) {
+      const responseData = await response.json();
+      return responseData;
+    } else {
+      console.error("Request code " + response.status);
+      throw new Error("Request failed with status code " + response.status);
     }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; 
+  }
 }
 
 
